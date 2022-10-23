227bd184fefc319e2bf0e5170b98296e17efff1a
Adam <897017+aSemy@users.noreply.github.com>
2022-10-23 14:47:28 +0200

    set java/kotlin versions in build-logic/build.gradle.kts

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
    `kotlin-dsl`
+   kotlin("jvm") version embeddedKotlinVersion
 }
 
 dependencies {
@@ -8,4 +11,24 @@ dependencies {
    implementation(libs.gradlePlugin.kotlinSerialization)
    implementation(libs.gradlePlugin.testlogger)
    implementation(libs.gradlePlugin.kotest)
+}
+
+val buildLogicJvmVersion = "11"
+val buildLogicJvmTarget = "1.8"
+
+
+kotlin {
+   jvmToolchain {
+      (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(buildLogicJvmVersion))
+   }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+   kotlinOptions {
+      jvmTarget = buildLogicJvmTarget
+   }
+}
+
+kotlinDslPluginOptions {
+   jvmTarget.set(buildLogicJvmTarget)
 }
