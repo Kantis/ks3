e0d18c8e477f76cdc3767d72bd4cba35c823c67a
Adam <897017+aSemy@users.noreply.github.com>
2022-10-23 11:18:32 +0200

    refactor publishing convention
    - remove dependency on java-lib
    - stub javadoc jar (it's easier to include source code instead)
    - group & version moved to base-convention
    - add project-local test dir
    - various tweaks to work around Gradle weirdness

--- build-logic/src/main/kotlin/ks3-publishing-conventions.gradle.kts
+++ build-logic/src/main/kotlin/ks3-publishing-conventions.gradle.kts
@@ -1,93 +1,103 @@
 plugins {
    signing
-   `java-library`
    `maven-publish`
    id("ks3-base-conventions")
 }
 
-val publications: PublicationContainer = (extensions.getByName("publishing") as PublishingExtension).publications
-
-val javadoc = tasks.named("javadoc")
-
-group = "io.ks3"
-version = "0.0.1-SNAPSHOT"
-
-val javadocJar by tasks.creating(Jar::class) {
+val javadocJarStub by tasks.registering(Jar::class) {
    group = JavaBasePlugin.DOCUMENTATION_GROUP
-   description = "Assembles java doc to jar"
+   description = "Empty Javadoc Jar (required by Maven Central)"
    archiveClassifier.set("javadoc")
-   from(javadoc)
 }
 
-publishing {
-   publications.withType<MavenPublication>().forEach {
-      it.apply {
-         artifact(javadocJar)
-      }
-   }
-}
-
-val ossrhUsername: String by project
-val ossrhPassword: String by project
+// it would be nicer to change the environment variables to be ORG_GRADLE_PROJECT_ossrhUsername and ORG_GRADLE_PROJECT_ossrhPassword
+val ossrhUsername = providers.environmentVariable("OSSRH_USERNAME").orElse(providers.gradleProperty("ossrhUsername"))
+val ossrhPassword = providers.environmentVariable("OSSRH_PASSWORD").orElse(providers.gradleProperty("ossrhPassword"))
 val signingKey: String? by project
 val signingPassword: String? by project
+
+
+val isSnapshotVersion = provider { version.toString().endsWith("SNAPSHOT") }
+
+val sonatypeReleaseUrl = isSnapshotVersion.map { isSnapshot ->
+   if (isSnapshot) {
+      "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+   } else {
+      "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+   }
+}
 
 signing {
    useGpgCmd()
    if (signingKey != null && signingPassword != null) {
-      @Suppress("UnstableApiUsage")
       useInMemoryPgpKeys(signingKey, signingPassword)
+      // Gradle hasn't updated the signing plugin to be compatible with lazy-configuration,
+      // so it needs weird workarounds: https://github.com/gradle/gradle/issues/19903
+      sign(closureOf<SignOperation> { sign(publishing.publications) })
    }
-   // TODO
-//    if (Ci.isRelease) {
-//        sign(publications)
-//    }
 }
 
 publishing {
    repositories {
-      maven {
-//            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-         val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-         name = "deploy"
-//            url = if (Ci.isRelease) releasesRepoUrl else snapshotsRepoUrl
-         url = snapshotsRepoUrl
-         credentials {
-            username = System.getenv("OSSRH_USERNAME") ?: ossrhUsername
-            password = System.getenv("OSSRH_PASSWORD") ?: ossrhPassword
+      if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
+         maven(sonatypeReleaseUrl) {
+            name = "SonatypeRelease"
+            credentials {
+               username = ossrhUsername.get()
+               password = ossrhPassword.get()
+            }
          }
+      }
+
+      // Publish to a project-local Maven directory, for verification. To test, run:
+      // ./gradlew publishAllPublicationsToMavenProjectLocalRepository
+      // and check $rootDir/build/maven-project-local
+      maven(rootProject.layout.buildDirectory.dir("maven-project-local")) {
+         name = "MavenProjectLocal"
       }
    }
 
-   publications.withType<MavenPublication>().forEach {
-      it.apply {
-         // if (Ci.isRelease)
-         pom {
-            name.set("Ks3")
-            description.set("KotlinX Serialization standard serializers")
+   publications.withType<MavenPublication>().configureEach {
+
+      artifact(javadocJarStub)
+
+      // if (Ci.isRelease)
+      pom {
+         name.set("Ks3")
+         description.set("KotlinX Serialization standard serializers")
+         url.set("https://github.com/Kantis/ks3")
+
+         scm {
+            connection.set("scm:git:https://github.com/Kantis/ks3/")
+            developerConnection.set("scm:git:https://github.com/Kantis/")
             url.set("https://github.com/Kantis/ks3")
+         }
 
-            scm {
-               connection.set("scm:git:https://github.com/Kantis/ks3/")
-               developerConnection.set("scm:git:https://github.com/Kantis/")
-               url.set("https://github.com/Kantis/ks3")
+         licenses {
+            license {
+               name.set("Apache-2.0")
+               url.set("https://opensource.org/licenses/Apache-2.0")
             }
+         }
 
-            licenses {
-               license {
-                  name.set("Apache-2.0")
-                  url.set("https://opensource.org/licenses/Apache-2.0")
-               }
-            }
-
-            developers {
-               developer {
-                  id.set("Kantis")
-                  name.set("Emil Kantis")
-                  email.set("emil.kantis@protonmail.com")
-               }
+         developers {
+            developer {
+               id.set("Kantis")
+               name.set("Emil Kantis")
+               email.set("emil.kantis@protonmail.com")
             }
          }
       }
+   }
+}
+
+tasks.withType<AbstractPublishToMaven>().configureEach {
+   // Gradle warns about some signing tasks using publishing task outputs without explicit dependencies.
+   // Here's a quick fix.
+   dependsOn(tasks.withType<Sign>())
+   mustRunAfter(tasks.withType<Sign>())
+
+   doLast {
+      logger.lifecycle("[task: ${this@configureEach.path}] ${publication.groupId}:${publication.artifactId}:${publication.version}")
    }
 }
