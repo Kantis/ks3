b60e842f10f2c176d5e49263ccbe94977bc00e07
Adam <897017+aSemy@users.noreply.github.com>
2022-10-23 17:24:51 +0200

    remove dummy tests

--- ks3-jdk/src/jvmTest/kotlin/DummyTest.kt
+++
@@ -1,8 +0,0 @@
-import kotlin.test.Test
-
-class DummyTest {
-//   @Test
-   fun `dummy test`() {
-      // prevent: No matching tests found in any candidate test task
-   }
-}
--- ks3-standard/src/commonTest/kotlin/DummyTest.kt
+++
@@ -1,8 +0,0 @@
-import kotlin.test.Test
-
-class DummyTest {
-//   @Test
-   fun dummyTest() {
-      // prevent: No matching tests found in any candidate test task
-   }
-}
