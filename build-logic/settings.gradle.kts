1476240bd4f4a22dbcd812e67b9f453a4fc25c65
Adam <897017+aSemy@users.noreply.github.com>
2022-10-23 15:09:49 +0200

    fix version catalog in build-logic

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,3 +1,12 @@
 rootProject.name = "build-logic"
 
 apply(from = "repositories.gradle.kts")
+
+dependencyResolutionManagement {
+   @Suppress("UnstableApiUsage")
+   versionCatalogs {
+      create("libs") {
+         from(files("../gradle/libs.versions.toml"))
+      }
+   }
+}
