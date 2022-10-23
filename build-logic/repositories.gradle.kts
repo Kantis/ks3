f27af020bd3238d44dd2f3620bda3ffb1c5cfcde
Adam <897017+aSemy@users.noreply.github.com>
2022-10-23 15:41:36 +0200

    try fixing Kotlin Native repo

--- build-logic/repositories.gradle.kts
+++ build-logic/repositories.gradle.kts
@@ -4,7 +4,7 @@
 @Suppress("UnstableApiUsage") // Central declaration of repositories is an incubating feature
 dependencyResolutionManagement {
 
-//   repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+   repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
 
    repositories {
       mavenCentral()
@@ -22,6 +22,22 @@ dependencyResolutionManagement {
          metadataSources { artifact() }
          content { includeModule("com.yarnpkg", "yarn") }
       }
+
+      listOf(
+         "osx-x86_64",
+         "osx-aarch64",
+         "linux-x86_64",
+         "windows-x86_64",
+      ) { os ->
+         // https://download.jetbrains.com/kotlin/native/builds/releases/1.7.20/linux-x86_64/kotlin-native-prebuilt-linux-x86_64-1.7.20.tar.gz
+         // https://download.jetbrains.com/kotlin/native/builds/releases/1.7.20/windows-x86_64/kotlin-native-prebuilt-windows-x86_64-1.7.20.zip
+         ivy("https://download.jetbrains.com/kotlin/native/builds/releases") {
+            name = "Kotlin Native - $os"
+            patternLayout { artifact("[revision]/$os/[artifact]-[os].[ext]") }
+            metadataSources { artifact() }
+         }
+      }
+
       sonatypeSnapshots()
       google()
       gradlePluginPortal() // tvOS builds need to be able to fetch a Kotlin Gradle plugin
