12f8b226da22388c90ac60d07388d603cd8a3186
Adam <897017+aSemy@users.noreply.github.com>
2022-10-29 12:01:49 +0200

    convert ks3-jdk to kotlin-jvm only project

--- ks3-jdk/build.gradle.kts
+++ ks3-jdk/build.gradle.kts
@@ -1,44 +1,27 @@
 plugins {
-   id("ks3.conventions.lang.kotlin-multiplatform")
+   id("ks3.conventions.lang.kotlin-jvm")
    id("ks3.conventions.publishing.maven-publish")
 }
 
-kotlin {
-   sourceSets {
-      val commonMain by getting {
-         dependencies {
-            implementation(platform(libs.kotlin.bom))
-            implementation(platform(libs.kotlinxSerialization.bom))
+dependencies {
+   implementation(platform(libs.kotlin.bom))
+   implementation(platform(libs.kotlinxSerialization.bom))
 
-            implementation(projects.ks3Standard)
+   implementation(projects.ks3Standard)
 
-            implementation(libs.kotlinxSerialization.core)
-         }
-      }
+   implementation(libs.kotlinxSerialization.core)
 
-      val commonTest by getting {
-         dependencies {
-            implementation(platform(libs.kotest.bom))
+   testImplementation(platform(libs.kotest.bom))
 
-            implementation(kotlin("test"))
+   testImplementation(kotlin("test"))
 
-            implementation(projects.ks3Test)
+   testImplementation(projects.ks3Test)
 
-            implementation(libs.kotest.frameworkEngine)
-            implementation(libs.kotest.assertionsCore)
-            implementation(libs.kotest.assertionsJson)
-            implementation(libs.kotest.property)
+   testImplementation(libs.kotest.frameworkEngine)
+   testImplementation(libs.kotest.assertionsCore)
+   testImplementation(libs.kotest.assertionsJson)
+   testImplementation(libs.kotest.property)
 
-            implementation(libs.kotlinxSerialization.json)
-         }
-      }
-
-      if (ks3Settings.enableKotlinJvm.get()) {
-         val jvmTest by getting {
-            dependencies {
-               implementation(libs.kotest.runnerJunit5)
-            }
-         }
-      }
-   }
+   testImplementation(libs.kotlinxSerialization.json)
+   testImplementation(libs.kotest.runnerJunit5)
 }
