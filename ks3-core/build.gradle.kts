813336e1db2b5e80027ab0d6142daa287cd2a018
Emil Kantis <emil.kantis@protonmail.com>
2023-07-27 15:02:53 +0200

    Enabling publish of ks3-core

--- ks3-core/build.gradle.kts
+++ ks3-core/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
    id("ks3.conventions.lang.kotlin-multiplatform-js")
    id("ks3.conventions.lang.kotlin-multiplatform-jvm")
    id("ks3.conventions.lang.kotlin-multiplatform-native")
+   id("ks3.conventions.publishing.maven-publish")
 }
 
 kotlin {
