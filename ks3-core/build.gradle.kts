f60d089772521502a995b5ed4d0ceea4cced1600
Emil Kantis <emil.kantis@protonmail.com>
2023-12-03 00:10:19 +0100

    removing unnecessary optin (ks3 base conventions already added this opt in)

--- ks3-core/build.gradle.kts
+++ ks3-core/build.gradle.kts
@@ -4,13 +4,3 @@ plugins {
    id("ks3.conventions.lang.kotlin-multiplatform-native")
    id("ks3.conventions.publishing.maven-publish")
 }
-
-kotlin {
-   targets.configureEach {
-      compilations.all {
-         kotlinOptions {
-            freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn")
-         }
-      }
-   }
-}
