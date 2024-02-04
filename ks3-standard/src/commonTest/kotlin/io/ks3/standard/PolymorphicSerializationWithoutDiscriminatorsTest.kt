c87b6c0b115df1b1f189df78183ec7167e44de15
Emil Kantis <emil.kantis@protonmail.com>
2024-02-04 21:59:20 +0100

    Switching spec to StringSpec (and removing nesting of tests)

--- ks3-standard/src/commonTest/kotlin/io/ks3/standard/PolymorphicSerializationWithoutDiscriminatorsTest.kt
+++ ks3-standard/src/commonTest/kotlin/io/ks3/standard/PolymorphicSerializationWithoutDiscriminatorsTest.kt
@@ -1,23 +1,21 @@
 package io.ks3.standard
 
-import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
 
-class PolymorphicSerializationWithoutDiscriminatorsTest : DescribeSpec(
+class PolymorphicSerializationWithoutDiscriminatorsTest : StringSpec(
    {
       val fooWithoutTypeSerializer = typeOmittingTransformer<Foo>()
       val json = Json
 
-      describe("PolymorphicSerializationWithoutDiscriminatorsTest") {
-         it("should do something") {
-            json.encodeToString(
-               ListSerializer(fooWithoutTypeSerializer),
-               listOf(Foo.Bar("bar"), Foo.Baz(1)),
-            ) shouldBe """[{"bar":"bar"},{"baz":1}]"""
-         }
+      "should omit type when serializing" {
+         json.encodeToString(
+            ListSerializer(fooWithoutTypeSerializer),
+            listOf(Foo.Bar("bar"), Foo.Baz(1)),
+         ) shouldBe """[{"bar":"bar"},{"baz":1}]"""
       }
    },
 ) {
