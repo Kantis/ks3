029f9a9d0bee8b7dc41fc0e2c2b5a51420d9cc76
Emil Kantis <emil.kantis@protonmail.com>
2024-02-04 22:00:26 +0100

    Switching spec to StringSpec (and removing nesting of tests)

--- ks3-standard/src/commonTest/kotlin/io/ks3/standard/ResilientListDeserializationTests.kt
+++ ks3-standard/src/commonTest/kotlin/io/ks3/standard/ResilientListDeserializationTests.kt
@@ -1,58 +1,54 @@
 package io.ks3.standard
 
-import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
-class ResilientListDeserializationTests : DescribeSpec(
+class ResilientListDeserializationTests : StringSpec(
    {
       val json = Json
 
-      describe("Deserializing bad elements should not fail the entire list") {
-         it("Single bad element is discarded") {
-            json.decodeFromString(resilientListSerializer<Int>(), "[1,2,3, \"foo\",5]") shouldBe listOf(1, 2, 3, 5)
-         }
-
-         it("Entire list can be discarded") {
-            json.decodeFromString(resilientListSerializer<Int>(), "[\"foo\",\"bar\"]") shouldBe listOf()
-         }
+      "Single bad element is discarded" {
+         json.decodeFromString(resilientListSerializer<Int>(), "[1,2,3, \"foo\",5]") shouldBe listOf(1, 2, 3, 5)
       }
 
-      describe("It should work with more complex structures") {
-         it("Should work with nested lists") {
-            json.decodeFromString(
-               // We need to pass the inner serializer so resilience will be applied within the nested list deserialization
-               resilientListSerializer<List<Int>>(
-                  resilientListSerializer<Int>(),
-               ),
-               "[[1,2,3], [4,\"foo\",6], [7,8,9]]",
-            ) shouldBe
-               listOf(
-                  listOf(1, 2, 3),
-                  listOf(4, 6),
-                  listOf(7, 8, 9),
-               )
-         }
+      "Entire list can be discarded" {
+         json.decodeFromString(resilientListSerializer<Int>(), "[\"foo\",\"bar\"]") shouldBe listOf()
+      }
 
-         it("Should omit data classes when they fail to deserialize") {
-            @Serializable
-            data class Foo(
-               val bar: String,
-               val baz: Int,
+      "Should work with nested lists" {
+         json.decodeFromString(
+            // We need to pass the inner serializer so resilience will be applied within the nested list deserialization
+            resilientListSerializer<List<Int>>(
+               resilientListSerializer<Int>(),
+            ),
+            "[[1,2,3], [4,\"foo\",6], [7,8,9]]",
+         ) shouldBe
+            listOf(
+               listOf(1, 2, 3),
+               listOf(4, 6),
+               listOf(7, 8, 9),
             )
+      }
 
-            json.decodeFromString(
-               resilientListSerializer<Foo>(),
-               """
-               [
-                  {"bar": "bar", "baz":1},
-                  {"bar": "bar"},
-                  {"bar": "bar", "baz": "x"}
-               ]
-               """.trimIndent(),
-            ) shouldBe listOf(Foo("bar", 1))
-         }
+      "Should omit data classes when they fail to deserialize" {
+         @Serializable
+         data class Foo(
+            val bar: String,
+            val baz: Int,
+         )
+
+         json.decodeFromString(
+            resilientListSerializer<Foo>(),
+            """
+            [
+               {"bar": "bar", "baz":1},
+               {"bar": "bar"},
+               {"bar": "bar", "baz": "x"}
+            ]
+            """.trimIndent(),
+         ) shouldBe listOf(Foo("bar", 1))
       }
    },
 )
