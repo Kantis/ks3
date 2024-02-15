a47aa60668791bdfb578090cf28bccb69cf4fb94
Emil Kantis <emil.kantis@protonmail.com>
2024-02-16 00:12:22 +0100

    Adding LenientJsonArraySerializer (#103)
    * Adding resilient list deserializer
    
    * Switching spec to StringSpec (and removing nesting of tests)
    
    * Implementing feedback
    
    * Updating name
    
    * adding doc

---
+++ ks3-standard/src/commonMain/kotlin/io/ks3/standard/LenientJsonArraySerializer.kt
@@ -0,0 +1,52 @@
+package io.ks3.standard
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.serializer
+
+/**
+ * Creates a serializer that will parse a JSON array into a list of [T] elements, ignoring any elements that fail to parse.
+ *
+ * Example:
+ * ```
+ * val lenientIntListSerializer = lenientJsonArraySerializer<Int>()
+ * val numbers = Json.decodeFromString(lenientIntListSerializer, "[1,2,3, \"foo\",5]")
+ *
+ * println(numbers) // > [1, 2, 3, 5]
+ * ```
+ */
+inline fun <reified T> lenientJsonArraySerializer(explicitElementSerializer: KSerializer<T>? = null) =
+   object : KSerializer<List<T>> {
+      private val elementSerializer = explicitElementSerializer ?: serializer<T>()
+      private val serializer: SerializationStrategy<List<T>> = ListSerializer(elementSerializer)
+      override val descriptor: SerialDescriptor = serializer.descriptor
+
+      override fun deserialize(decoder: Decoder): List<T> {
+         require(decoder is JsonDecoder) { "This deserializer can only be used with Json" }
+         val json = decoder.json
+         val list = decoder.decodeJsonElement()
+         return list
+            .jsonArray
+            .mapNotNull {
+               try {
+                  json.decodeFromJsonElement(elementSerializer, it)
+               } catch (e: SerializationException) {
+                  null
+               } catch (e: IllegalArgumentException) {
+                  null
+               }
+            }
+      }
+
+      override fun serialize(
+         encoder: Encoder,
+         value: List<T>,
+      ): Unit = serializer.serialize(encoder, value)
+   }
---
+++ ks3-standard/src/commonTest/kotlin/io/ks3/standard/LenientJsonArraySerializerTest.kt
@@ -0,0 +1,54 @@
+package io.ks3.standard
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+class LenientJsonArraySerializerTest : StringSpec(
+   {
+      val json = Json
+
+      "Single bad element is discarded" {
+         json.decodeFromString(lenientJsonArraySerializer<Int>(), "[1,2,3, \"foo\",5]") shouldBe listOf(1, 2, 3, 5)
+      }
+
+      "Entire list can be discarded" {
+         json.decodeFromString(lenientJsonArraySerializer<Int>(), "[\"foo\",\"bar\"]") shouldBe listOf()
+      }
+
+      "Should work with nested lists" {
+         json.decodeFromString(
+            // We need to pass the inner serializer so resilience will be applied within the nested list deserialization
+            lenientJsonArraySerializer<List<Int>>(
+               lenientJsonArraySerializer<Int>(),
+            ),
+            "[[1,2,3], [4,\"foo\",6], [7,8,9]]",
+         ) shouldBe
+            listOf(
+               listOf(1, 2, 3),
+               listOf(4, 6),
+               listOf(7, 8, 9),
+            )
+      }
+
+      "Should omit data classes when they fail to deserialize" {
+         @Serializable
+         data class Foo(
+            val bar: String,
+            val baz: Int,
+         )
+
+         json.decodeFromString(
+            lenientJsonArraySerializer<Foo>(),
+            """
+            [
+               {"bar": "bar", "baz":1},
+               {"bar": "bar"},
+               {"bar": "bar", "baz": "x"}
+            ]
+            """.trimIndent(),
+         ) shouldBe listOf(Foo("bar", 1))
+      }
+   },
+)
