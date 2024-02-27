1968dbfc3f6b214e4d5d9401e4a4d6f665e1fec4
Emil Kantis <emil.kantis@protonmail.com>
2024-02-27 23:03:03 +0100

    Renaming typealias package so it doesn't require backticks (#114)
    * Renaming typealias package so it doesn't require backticks
    
    Fixes #113
    
    * Updating wording of deprecation

--- ks3-jdk/src/jvmMain/kotlin/io/ks3/java/typealias/Typealiases.kt
+++ ks3-jdk/src/jvmMain/kotlin/io/ks3/java/typealias/Typealiases.kt
@@ -26,49 +26,93 @@ import java.time.YearMonth
 import java.util.UUID
 
 // -- IO --
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("FileAsString", "io.ks3.java.typealiases.FileAsString"),
+)
 typealias FileAsString =
    @Serializable(with = FilePathSerializer::class)
    File
 
 // -- NET --
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("UrlAsString", "io.ks3.java.typealiases.UrlAsString"),
+)
 typealias UrlAsString =
    @Serializable(with = UrlSerializer::class)
    URL
 
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("UriAsString", "io.ks3.java.typealiases.UriAsString"),
+)
 typealias UriAsString =
    @Serializable(with = UriSerializer::class)
    URI
 
 // -- TIME --
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("YearMonthAsString", "io.ks3.java.typealiases.YearMonthAsString"),
+)
 typealias YearMonthAsString =
    @Serializable(with = YearMonthAsStringSerializer::class)
    YearMonth
 
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("InstantAsString", "io.ks3.java.typealiases.InstantAsString"),
+)
 typealias InstantAsString =
    @Serializable(with = InstantAsStringSerializer::class)
    Instant
 
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("InstantAsLong", "io.ks3.java.typealiases.InstantAsLong"),
+)
 typealias InstantAsLong =
    @Serializable(with = InstantAsLongSerializer::class)
    Instant
 
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("LocalDateAsString", "io.ks3.java.typealiases.LocalDateAsString"),
+)
 typealias LocalDateAsString =
    @Serializable(with = LocalDateAsStringSerializer::class)
    LocalDate
 
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("LocalDateTimeAsString", "io.ks3.java.typealiases.LocalDateTimeAsString"),
+)
 typealias LocalDateTimeAsString =
    @Serializable(with = LocalDateTimeAsStringSerializer::class)
    LocalDateTime
 
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("LocalTimeAsString", "io.ks3.java.typealiases.LocalTimeAsString"),
+)
 typealias LocalTimeAsString =
    @Serializable(with = LocalTimeAsStringSerializer::class)
    LocalTime
 
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("OffsetDateTimeAsString", "io.ks3.java.typealiases.OffsetDateTimeAsString"),
+)
 typealias OffsetDateTimeAsString =
    @Serializable(with = OffsetDateTimeAsStringSerializer::class)
    OffsetDateTime
 
 // -- UTIL --
+@Deprecated(
+   "Changed package to io.ks3.java.typealiases to avoid need for backticks in imports. This package will be removed in 1.0",
+   ReplaceWith("UuidAsString", "io.ks3.java.typealiases.UuidAsString"),
+)
 typealias UuidAsString =
    @Serializable(with = UuidSerializer::class)
    UUID
